8c842aaa14678598350314167ae42506430b534e
soundar-1998 <soundark221@gmail.com>
2024-12-24 00:35:12 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express =require('express');
+const app=express();
+const mongoose=require('mongoose');
+const cors= require('cors');
+const routerurl=require('./router/router')
+app.use(express.json());
+app.use(cors())
+app.use("/app",routerurl);
+//final25 is a database name
+if(mongoose.connect("mongodb+srv://user-soundar:soundar2704@cluster0.17hrs.mongodb.net/Mern_stack?retryWrites=true&w=majority&appName=Cluster0"));
+//mongodb://127.0.0.1:27017/MERN
+//
+//mongodb+srv://rkalai92:Kalai_12345@cluster0.ev42l.mongodb.net/Customer-details?retryWrites=true&w=majority&appName=Cluster0
+{
+    console.log('database is connected ');
+
+}   
+
+
+app.listen(4000 ,()=>{
+    console.log('server is running on port  4000');
+})
+
+//https://cars-customerdetails.onrender.com
